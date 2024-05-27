38540de988001f1ae96ec2db20d36bab1d1f1129
Marcell Perger <marcell.perger.1@gmail.com>
2024-05-27 20:36:34 +0100

    refactor: Convert back to checked exceptions
    Checked exceptions are much more ergonomic has they have first-class language support and syntax, unlike my Result type.

--- src/main/java/net/marcellperger/mathexpr/parser/Parser.java
+++ src/main/java/net/marcellperger/mathexpr/parser/Parser.java
@@ -3,9 +3,6 @@ package net.marcellperger.mathexpr.parser;
 import net.marcellperger.mathexpr.*;
 import net.marcellperger.mathexpr.util.Pair;
 import net.marcellperger.mathexpr.util.Util;
-import net.marcellperger.mathexpr.util.VoidVal;
-import net.marcellperger.mathexpr.util.rs.Option;
-import net.marcellperger.mathexpr.util.rs.Result;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Range;
@@ -14,8 +11,6 @@ import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.function.Function;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
@@ -42,95 +37,31 @@ public class Parser {
     public MathSymbol parseExpr() throws ExprParseException {
         return parseInfixPrecedenceLevel(SymbolInfo.MAX_PRECEDENCE);
     }
-    public Result<MathSymbol, ExprParseException> parseExpr_result() {
-        // TODO do this better
-        try {
-            return Result.newOk(parseInfixPrecedenceLevel(SymbolInfo.MAX_PRECEDENCE));
-        } catch (ExprParseException e) {
-            return Result.newErr(e);
-        }
-    }
 
     // https://regex101.com/r/2EogTA/1
     protected static final Pattern DOUBLE_RE = Pattern.compile("^([+-]?)(\\d*\\.\\d+|\\d+\\.?)(?:[eE]([+-]?\\d+))?");
-    @SuppressWarnings("unused")  // ignore for now - will fix later with the better error handling
-    public @NotNull MathSymbol parseDoubleLiteral_exc() throws ExprParseException {
-        return switch (parseDoubleLiteral_null()) {
-            case null -> throw new ExprParseException("Couldn't parse double in expression");
-            case MathSymbol sym -> sym;
-        };
-    }
-    public @Nullable MathSymbol parseDoubleLiteral_null() {
+    public @NotNull MathSymbol parseDoubleLiteral() throws ExprParseException {
         discardWhitespace();
-        Matcher m = DOUBLE_RE.matcher(strFromHere());
-        if (!m.lookingAt()) return null;
-        String s = m.group();
-        idx += s.length();
-        double value;
+        String s = matchNextRegexString(DOUBLE_RE, "Invalid number (double)");
         try {
-            value = Double.parseDouble(s);
-        } catch (NumberFormatException _exc) {
-            // Technically this should never happen - assuming I've got that regex right
-            assert false: "There is a problem with the regex, this should've been rejected earlier";
-            return null;
+            return new BasicDoubleSymbol(Double.parseDouble(s));
+        } catch (NumberFormatException exc) {
+            throw new AssertionError("There is a problem with the regex," +
+                " this should've been rejected earlier", exc);
         }
-        return new BasicDoubleSymbol(value);
     }
-    public @NotNull Result<MathSymbol, ExprParseException> parseDoubleLiteral_result() {
+
+    public @NotNull MathSymbol parseParensOrLiteral() throws ExprParseException {
         discardWhitespace();
-        return matchNextRegexString(DOUBLE_RE, "Invalid number").andThen(s -> {
-            try {
-                return Result.newOk(new BasicDoubleSymbol(Double.parseDouble(s)));
-            } catch (NumberFormatException exc) {
-                throw new AssertionError("There is a problem with the regex," +
-                    " this should've been rejected earlier", exc);
-            }
-        });
+        return peek() == '(' ? parseParens() : parseDoubleLiteral();
     }
 
-    public @NotNull Result<MathSymbol, ExprParseException> parseParensOrLiteral_result() {
-        discardWhitespace();
-        return peek() == '(' ? parseParens_result() : parseDoubleLiteral_result();
-    }
-
-    public @NotNull Result<MathSymbol, ExprParseException> parseParens_result() {
-        return advanceExpectNext_ignoreWs_result('(')
-            .andThen(_c -> parseExpr_result())
-            .runIfOk(_sym -> advanceExpectNext_ignoreWs_result(')'));
-    }
-
-    public @Nullable MathSymbol parseParensOrLiteral() throws ExprParseException {
-        discardWhitespace();
-        if(peek() == '(') return parseParens();
-        // TODO add a better error handling system - don't want to maintain 2 versions of each function
-        //  returning null: +easier to do unions, +no need for verbose try/catch, -no info about errors
-        return parseDoubleLiteral_null();
-    }
-
-    public @Nullable MathSymbol parseParens() throws ExprParseException {
+    public MathSymbol parseParens() throws ExprParseException {
         advanceExpectNext_ignoreWs('(');
         MathSymbol sym = parseExpr();
         advanceExpectNext_ignoreWs(')');
         return sym;
     }
-
-//    public Result<MathSymbol, ExprParseException> parseInfixPrecedenceLevel_result(int level) {
-//        discardWhitespace();
-//        if(level == 0) return parseParensOrLiteral_result();
-//        PrecedenceLevelInfo precInfo = SymbolInfo.PREC_LEVELS_INFO.get(level);
-//        if (precInfo == null) return parseInfixPrecedenceLevel_result(level - 1);  // fallthrough empty levels
-//        return parseInfixPrecedenceLevel_result(level - 1).andThen(left -> switch (precInfo.dirn) {
-//            case LeftToRight -> parseInfixPrecedenceLevel_LTR(left, precInfo);
-//            case RightToLeft -> parseInfixPrecedenceLevel_RTL(left, precInfo);
-//            case null -> parseInfixPrecedenceLevel_noDirn(left, precInfo);
-//        });
-////        MathSymbol left = parseInfixPrecedenceLevel(level - 1);
-////        return switch (precInfo.dirn) {
-////            case LeftToRight -> parseInfixPrecedenceLevel_LTR(left, precInfo);
-////            case RightToLeft -> parseInfixPrecedenceLevel_RTL(left, precInfo);
-////            case null -> parseInfixPrecedenceLevel_noDirn(left, precInfo);
-////        };
-//    }
 
     public MathSymbol parseInfixPrecedenceLevel(int level) throws ExprParseException {
         discardWhitespace();
@@ -143,22 +74,6 @@ public class Parser {
             case RightToLeft -> parseInfixPrecedenceLevel_RTL(left, precInfo);
             case null -> parseInfixPrecedenceLevel_noDirn(left, precInfo);
         };
-    }
-
-    private Result<MathSymbol, ExprParseException> parseInfixPrecedenceLevel_RTL_result(MathSymbol left, PrecedenceLevelInfo precInfo) {
-        return Result.fromTry(() -> {
-            String op;
-            List<Pair<SymbolInfo, MathSymbol>> otherOps = new ArrayList<>();
-            while((op = discardMatchesNextAny_optionsSorted_removeWs(precInfo.sortedInfixes)) != null) {
-                otherOps.add(new Pair<>(
-                    Util.getNotNull(precInfo.infixToSymbolMap, op),
-                    parseInfixPrecedenceLevel(precInfo.precedence - 1)));
-            }
-            return otherOps.reversed().stream().reduce((rightpair, leftpair) ->
-                leftpair.asVars((preOp, argL) ->
-                    new Pair<>(preOp, rightpair.asVars((midOp, argR) -> BinaryOperation.construct(argL, midOp, argR))))
-            ).map(p -> p.asVars((midOp, argR) -> BinaryOperation.construct(left, midOp, argR))).orElse(left);
-        }, ExprParseException.class);
     }
 
     private MathSymbol parseInfixPrecedenceLevel_RTL(MathSymbol left, PrecedenceLevelInfo precInfo) throws ExprParseException {
@@ -237,45 +152,39 @@ public class Parser {
         advanceWhile(Character::isWhitespace);
     }
 
-    protected void advanceExpectNext(char expected) {
+    protected void advanceExpectNext(char expected) throws ExprParseException {
         char actual = advance();
-        if(actual != expected) throw new ExprParseRtException("Expected '%c', got '%c'".formatted(expected, actual));
+        if(actual != expected) throw new ExprParseException("Expected '%c', got '%c'".formatted(expected, actual));
     }
-    protected void advanceExpectNext_ignoreWs(char expected) {
+    protected void advanceExpectNext_ignoreWs(char expected) throws ExprParseException {
         discardWhitespace();
         advanceExpectNext(expected);
     }
-    protected Result<Character, ExprParseException> advanceExpectNext_ignoreWs_result(char expected) {
-        discardWhitespace();
-        return advanceExpectNext_result(expected);
-    }
-    protected Result<Character, ExprParseException> advanceExpectNext_result(char expected) {
-        char actual = advance();
-        if (actual == expected) return Result.newOk(actual);
-        return Result.newErr(new ExprParseException("Expected '%c', got '%c'".formatted(expected, actual)));
-    }
 
-    protected Result<MatchResult, ExprParseException> matchNextRegexResult(@NotNull Pattern pat, ExprParseException exc) {
+    protected MatchResult matchNextRegexResult(@NotNull Pattern pat, ExprParseException exc) throws ExprParseException {
         Matcher m = pat.matcher(strFromHere());
-        if (!m.lookingAt()) return Result.fromExc(exc);
+        if (!m.lookingAt()) throw exc;
         String s = m.group();
         idx += s.length();
-        return Result.newOk(m.toMatchResult());
+        return m.toMatchResult();
     }
-    protected Result<MatchResult, ExprParseException> matchNextRegexResult(@NotNull Pattern pat, String exc) {
+    protected MatchResult matchNextRegexResult(@NotNull Pattern pat, String exc) throws ExprParseException {
         return matchNextRegexResult(pat, new ExprParseException(exc));
     }
-    protected Result<MatchResult, ExprParseException> matchNextRegexResult(@NotNull Pattern pat) {
+    protected MatchResult matchNextRegexResult(@NotNull Pattern pat) throws ExprParseException {
         return matchNextRegexResult(pat, "Regex should've been matched");
     }
-    protected Result<String, ExprParseException> matchNextRegexString(@NotNull Pattern pat, ExprParseException exc) {
-        return matchNextRegexResult(pat, exc).map(MatchResult::group);
+    @SuppressWarnings("unused")
+    protected String matchNextRegexString(@NotNull Pattern pat, ExprParseException exc) throws ExprParseException {
+        return matchNextRegexResult(pat, exc).group();
     }
-    protected Result<String, ExprParseException> matchNextRegexString(@NotNull Pattern pat, String msg) {
-        return matchNextRegexResult(pat, msg).map(MatchResult::group);
+    @SuppressWarnings("SameParameterValue")
+    protected String matchNextRegexString(@NotNull Pattern pat, String msg) throws ExprParseException {
+        return matchNextRegexResult(pat, msg).group();
     }
-    protected Result<String, ExprParseException> matchNextRegexString(@NotNull Pattern pat) {
-        return matchNextRegexResult(pat).map(MatchResult::group);
+    @SuppressWarnings("unused")
+    protected String matchNextRegexString(@NotNull Pattern pat) throws ExprParseException {
+        return matchNextRegexResult(pat).group();
     }
 
     protected boolean matchesNext(@NotNull String expected) {
