f06324dbb320f974dc4d55850705bdcad6dfc073
Marcell Perger <marcell.perger.1@gmail.com>
2024-06-03 22:11:00 +0100

    fix: Fix FP issues

--- src/main/java/net/marcellperger/mathexpr/util/MathUtil.java
+++ src/main/java/net/marcellperger/mathexpr/util/MathUtil.java
@@ -2,10 +2,12 @@ package net.marcellperger.mathexpr.util;
 
 import org.jetbrains.annotations.Range;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class MathUtil {
     protected MathUtil() {}
 
-    // TODO this doesn't work: roundToSigFigs(1.22222 + 0.3, 10) == 1.5222200000000001
     public static double roundToSigFigs(double value, @Range(from=1, to=Integer.MAX_VALUE) int sigFigs) {
         int mostSignificantDigit = (int)Math.floor(Math.log10(value));
         int roundToDigit = mostSignificantDigit - sigFigs + 1;
@@ -13,7 +15,9 @@ public class MathUtil {
     }
 
     public static double roundToNearest(double value, double nearest) {
-        return Math.round(value / nearest) * nearest;
+        BigDecimal nearestD = BigDecimal.valueOf(nearest);
+        return BigDecimal.valueOf(value).divide(nearestD, RoundingMode.HALF_UP)
+            .setScale(0, RoundingMode.HALF_UP).multiply(nearestD).doubleValue();
     }
     public static double roundToDP(double value, int decimalPlaces) {
         return roundToNearest(value, Math.pow(10, decimalPlaces));
