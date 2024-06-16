38dc05efaf482a18ab909029e89a68c23da2ff6b
Marcell Perger <marcell.perger.1@gmail.com>
2024-06-16 21:21:30 +0100

    test: Add more tests

--- src/test/java/net/marcellperger/mathexpr/cli/minicli/MiniCLITest.java
+++ src/test/java/net/marcellperger/mathexpr/cli/minicli/MiniCLITest.java
@@ -2,6 +2,7 @@ package net.marcellperger.mathexpr.cli.minicli;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.util.List;
 
@@ -32,7 +33,31 @@ class MiniCLITest {
             assertEquals("12", rounding.getValue());
             assertEquals(true, interactive.getValue());
             assertEquals(List.of("1+1 * 2"), cli.getPositionalArgs());
+            cli.parseArgs(new String[]{"--interactive=false", "1+1 * 2"});
+            assertEquals("12", rounding.getValue());
+            assertEquals(false, interactive.getValue());
+            assertEquals(List.of("1+1 * 2"), cli.getPositionalArgs());
+            cli.parseArgs(new String[]{"--interactive=true", "1+1 * 2"});
+            assertEquals("12", rounding.getValue());
+            assertEquals(true, interactive.getValue());
+            assertEquals(List.of("1+1 * 2"), cli.getPositionalArgs());
+            cli.parseArgs(new String[]{"-i=yes", "1+1 * 2"});
+            assertEquals("12", rounding.getValue());
+            assertEquals(true, interactive.getValue());
+            assertEquals(List.of("1+1 * 2"), cli.getPositionalArgs());
+            cli.parseArgs(new String[]{"-i=0", "1+1 * 2"});
+            assertEquals("12", rounding.getValue());
+            assertEquals(false, interactive.getValue());
+            assertEquals(List.of("1+1 * 2"), cli.getPositionalArgs());
+            cli.parseArgs(new String[]{"-i", "0"});
+            assertEquals("12", rounding.getValue());
+            assertEquals(true, interactive.getValue());
+            assertEquals(List.of("0"), cli.getPositionalArgs());
             cli.parseArgs(new String[]{"-R", "6", "1+1 * 2"});
+            assertEquals("6", rounding.getValue());
+            assertEquals(false, interactive.getValue());
+            assertEquals(List.of("1+1 * 2"), cli.getPositionalArgs());
+            cli.parseArgs(new String[]{ "1+1 * 2", "-R", "6"});
             assertEquals("6", rounding.getValue());
             assertEquals(false, interactive.getValue());
             assertEquals(List.of("1+1 * 2"), cli.getPositionalArgs());
@@ -45,9 +70,19 @@ class MiniCLITest {
             assertEquals(false, interactive.getValue());
             assertEquals(List.of("1+1 * 2"), cli.getPositionalArgs());
             // TODO 2nd arg cannot be -8 here as it is interpreted as a flag so
-            //  need to support `-` or need to see if this doesn't match any option (nut then adding an option would be breaking)
-            assertThrows(CLIParseException.class, () -> cli.parseArgs(new String[]{"1+2", "*8"}));
+            //  need to support `--` or need to see if this doesn't match any option
+            //  (but then adding an option would be a breaking change)
+            assertThrowsAndMsgContains(CLIParseException.class, "positional args",
+                () -> cli.parseArgs(new String[]{"1+2", "*8"}));
+            assertThrowsAndMsgContains(CLIParseException.class, "Bad boolean value",
+                () -> cli.parseArgs(new String[]{"--interactive=abc"}));
         }
     }
 
+    <T extends Throwable> void assertThrowsAndMsgContains(@SuppressWarnings("SameParameterValue") Class<T> cls,
+                                                          String containsMsg, Executable fn) {
+        T exc = assertThrows(cls, fn);
+        assertTrue(exc.getMessage().contains(containsMsg), () ->
+            "Expected error message ('%s') to contain %s".formatted(exc.getMessage(), containsMsg));
+    }
 }
